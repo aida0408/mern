9f51a204a63ea152b3b43ad61275e3e3b89ca76a
Bakazova Aida <solnyshko669@gmail.com>
2022-04-07 02:10:14 +0600

    initial

---
+++ routes/auth.routes.js
@@ -0,0 +1,18 @@
+const {Router} = require('express')
+const router = Router()
+
+router.post('/register', async (req, res) => {
+    try{
+        const {email, password} = req.body
+
+    } catch (e) {
+        res.status(500).json({message: 'Что-то пошло не так, попробуйте снова!'})
+    }
+
+
+})
+router.post('/login', async (req, res) => {
+
+})
+
+module.exports = router
